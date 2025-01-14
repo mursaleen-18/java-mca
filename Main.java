28e29f02064320f1e3e3bed9b448037107f4561a
Mohd Khan <oliwertwist9@gmail.com>
2025-01-14 14:21:52 +0530

    1st day

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String arg[]){
+        int a = 10, b = 20, c;
+        System.out.println("Arithmetic Operations");
+        c = a + b;
+        System.out.println("The sum of a & b = "+c);
+    }
+}
