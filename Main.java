950a1f309706791bfda3fd8033b07ff4605dbafc
mursaleen-18 <mmursaleenstb18@gmail.com>
2025-01-15 17:25:27 +0530

    Fixing Typos

--- Main.java
+++ Main.java
@@ -4,15 +4,15 @@ class Main {
     public static void main(String arg[]){
       Scanner askingNum = new Scanner(System.in);
 
-      System.out.println("Enter the length =");
+      System.out.println("Enter the length = ");
       double length = askingNum.nextDouble();
 
-      System.out.println("Enter the width =");
+      System.out.println("Enter the width = ");
       double width = askingNum.nextDouble();
 
       double area = calCulateArea(length,width);
 
-      System.out.println("The area od rectangle is = "+area);
+      System.out.println("The area of the rectangle is = "+area);
 
       askingNum.close();
 
